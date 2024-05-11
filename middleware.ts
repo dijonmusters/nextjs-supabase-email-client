6f90cb4e5560547b86bb2651d9508c0f845c82fd
Ali Hussein <42802922+Ali-Hussein-dev@users.noreply.github.com>
2024-05-11 10:48:18 +0200

    Enable multiple public paths
    It's likely that developers will have multiple public paths, so they can easily add new ones as needed.

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,34 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import { createClient } from './app/utils/supabase/middleware';
+import { NextResponse, type NextRequest } from 'next/server'
+import { createClient } from './app/utils/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
-  const { supabase, response } = createClient(request);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+// Add more public paths that do not require authentication
+const publicPaths = ["/login", "/"]
 
-  if (!user) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-  return response;
+/**
+ * middleware to refresh the user's session before loading Server Component routes. 
+ * This ensures that the session is up-to-date and the user is authenticated
+ */
+export async function middleware(req: NextRequest) {
+    try {
+        const { supabase, response } = createClient(req)
+
+        const { data: { user } } = await supabase.auth.getUser()
+        if (!user && !publicPaths.includes(req.nextUrl.pathname)) {
+            return NextResponse.redirect(new URL("/login", req.url))
+        }
+
+        return response
+    } catch (e) {
+        return NextResponse.next({
+            request: {
+                headers: req.headers,
+            },
+        })
+    }
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-};
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
+}
